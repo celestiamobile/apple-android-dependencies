246260649ee61fc845b10769fdcf7f6aef471264
Levin Li <lilinfeng303@outlook.com>
2023-07-10 20:28:18 +0800

    Update versions

--- download.sh
+++ download.sh
@@ -6,12 +6,12 @@ source versions.sh
 
 wget "https://naif.jpl.nasa.gov/pub/naif/toolkit/C/MacIntel_OSX_AppleC_64bit/packages/${CSPICE_VERSION}.tar.Z"
 
-wget "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.39/${LIBPNG_VERSION}.tar.xz"
+wget "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.40/${LIBPNG_VERSION}.tar.xz"
 
 JPEG_DOWNLOAD_NAME="jpegsrc.v9e.tar.gz"
 wget "https://www.ijg.org/files/${JPEG_DOWNLOAD_NAME}" -O "${JPEG_VERSION}.tar.gz"
 
-wget "https://downloads.sourceforge.net/project/freetype/freetype2/2.13.0/${FREETYPE_VERSION}.tar.xz"
+wget "https://downloads.sourceforge.net/project/freetype/freetype2/2.13.1/${FREETYPE_VERSION}.tar.xz"
 
 wget "https://ftp.gnu.org/gnu/gettext/${GETTEXT_VERSION}.tar.gz"
 
@@ -19,7 +19,7 @@ wget "https://www.lua.org/ftp/${LUA_VERSION}.tar.gz"
 
 wget "https://download.gnome.org/sources/libepoxy/1.5/${LIBEPOXY_VERSION}.tar.xz"
 
-FMT_DOWNLOAD_NAME="9.1.0.tar.gz"
+FMT_DOWNLOAD_NAME="10.0.0.tar.gz"
 wget "https://github.com/fmtlib/fmt/archive/${FMT_DOWNLOAD_NAME}" -O "${FMT_VERSION}.tar.gz"
 
 MESHOPTIMIZER_DOWNLOAD_NAME="v0.19.tar.gz"
@@ -27,8 +27,8 @@ wget "https://github.com/zeux/meshoptimizer/archive/refs/tags/${MESHOPTIMIZER_DO
 
 wget "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/${EIGEN_VERSION}.tar.gz"
 
-ICU_DOWNLOAD_NAME="icu4c-72_1-src.tgz"
-wget "https://github.com/unicode-org/icu/releases/download/release-72-1/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz"
+ICU_DOWNLOAD_NAME="icu4c-73_2-src.tgz"
+wget "https://github.com/unicode-org/icu/releases/download/release-73-2/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz"
 
 wget "https://github.com/google/breakpad/tarball/7a1a190f4f68e8a3e06788498f50a4d5520a69f3" -O "${BREAKPAD_VERSION}.tar.gz"
 
