60100ac87abc51558094fbec214947bf5d372840
Levin Li <lilinfeng303@outlook.com>
2024-04-08 14:06:09 -0700

    Update

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 . `pwd`/versions.sh
 
-wget "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.42/${LIBPNG_VERSION}.tar.xz" --no-check-certificate
+wget "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.43/${LIBPNG_VERSION}.tar.xz" --no-check-certificate
 
 #JPEG_DOWNLOAD_NAME="jpegsrc.v9e.tar.gz"
 #wget "https://www.ijg.org/files/${JPEG_DOWNLOAD_NAME}" -O "${JPEG_VERSION}.tar.gz" --no-check-certificate
@@ -15,7 +15,7 @@ wget "https://ftp.gnu.org/gnu/gettext/${GETTEXT_VERSION}.tar.gz" --no-check-cert
 
 wget "https://www.lua.org/ftp/${LUA_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/LuaJIT/LuaJIT/archive/c525bcb9024510cad9e170e12b6209aedb330f83.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/LuaJIT/LuaJIT/archive/d06beb0480c5d1eb53b3343e78063950275aa281.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://download.gnome.org/sources/libepoxy/1.5/${LIBEPOXY_VERSION}.tar.xz" --no-check-certificate
 
@@ -30,13 +30,13 @@ wget "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/${EIGEN_VERSION}.tar.gz"
 ICU_DOWNLOAD_NAME="icu4c-74_2-src.tgz"
 wget "https://github.com/unicode-org/icu/releases/download/release-74-2/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz" --no-check-certificate
 
-wget "https://github.com/google/breakpad/tarball/f032e4c3b4eaef64432482cb00eb0c3df33cde48" -O "${BREAKPAD_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/google/breakpad/tarball/e5dc1f86b2defc8f76b56fb473b804e42e75c41d" -O "${BREAKPAD_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp" --no-check-certificate
 
 wget "https://zlib.net/${ZLIB_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/3530768138c5ba3dfbb2c43c830493f632f7ea33" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/6fdfecda943798736fae0f3292528ece1c1b001c" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/KhronosGroup/EGL-Registry/tarball/7db3005d4c2cb439f129a0adc931f3274f9019e6" -O "${EGL_VERSION}.tar.gz" --no-check-certificate
 
@@ -44,7 +44,7 @@ wget "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.0.2/${J
 
 wget "https://codeload.github.com/mackron/miniaudio/tar.gz/refs/tags/0.11.21" -O "${MINIAUDIO_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/libsdl-org/SDL/releases/download/release-2.30.0/${SDL2_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/libsdl-org/SDL/releases/download/release-2.30.2/${SDL2_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/nih-at/libzip/releases/download/v1.10.1/${LIBZIP_VERSION}.tar.gz" --no-check-certificate
 
