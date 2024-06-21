4cfd23dd454220c1cc29129a0ffa6ef74794e511
Levin Li <lilinfeng303@outlook.com>
2024-06-20 21:24:21 -0700

    Bump versions

--- download.sh
+++ download.sh
@@ -15,7 +15,7 @@ wget "https://ftp.gnu.org/gnu/gettext/${GETTEXT_VERSION}.tar.gz" --no-check-cert
 
 wget "https://www.lua.org/ftp/${LUA_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/LuaJIT/LuaJIT/archive/d06beb0480c5d1eb53b3343e78063950275aa281.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/LuaJIT/LuaJIT/archive/93e87998b24021b94de8d1c8db244444c46fb6e9.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://download.gnome.org/sources/libepoxy/1.5/${LIBEPOXY_VERSION}.tar.xz" --no-check-certificate
 
@@ -27,24 +27,24 @@ wget "https://github.com/zeux/meshoptimizer/archive/refs/tags/${MESHOPTIMIZER_DO
 
 wget "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/${EIGEN_VERSION}.tar.gz" --no-check-certificate
 
-ICU_DOWNLOAD_NAME="icu4c-74_2-src.tgz"
-wget "https://github.com/unicode-org/icu/releases/download/release-74-2/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz" --no-check-certificate
+ICU_DOWNLOAD_NAME="icu4c-75_1-src.tgz"
+wget "https://github.com/unicode-org/icu/releases/download/release-75-1/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz" --no-check-certificate
 
-wget "https://github.com/google/breakpad/tarball/e5dc1f86b2defc8f76b56fb473b804e42e75c41d" -O "${BREAKPAD_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/google/breakpad/tarball/e0a3a76758874bdc04e7a32e254403332f515a2d" -O "${BREAKPAD_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp" --no-check-certificate
 
 wget "https://zlib.net/${ZLIB_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/6fdfecda943798736fae0f3292528ece1c1b001c" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/f26a7781c639aeb2666c48612afafeb1c7111f28" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/KhronosGroup/EGL-Registry/tarball/7db3005d4c2cb439f129a0adc931f3274f9019e6" -O "${EGL_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/KhronosGroup/EGL-Registry/tarball/d4f0a976e4ffe8493bdff5c3280ce13156286904" -O "${EGL_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.0.2/${JPEG_TURBO_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.0.3/${JPEG_TURBO_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://codeload.github.com/mackron/miniaudio/tar.gz/refs/tags/0.11.21" -O "${MINIAUDIO_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/libsdl-org/SDL/releases/download/release-2.30.2/${SDL2_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/libsdl-org/SDL/releases/download/release-2.30.4/${SDL2_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/nih-at/libzip/releases/download/v1.10.1/${LIBZIP_VERSION}.tar.gz" --no-check-certificate
 
@@ -52,4 +52,5 @@ wget "https://github.com/nih-at/libzip/releases/download/v1.10.1/${LIBZIP_VERSIO
 
 # wget "https://storage.googleapis.com/aom-releases/${AOM_VERSION}.tar.gz" -O "${AOM_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/boostorg/boost/releases/download/boost-1.84.0/${BOOST_VERSION}.tar.xz" --no-check-certificate
+BOOST_DOWNLOAD_NAME="boost-1.85.0-cmake"
+wget "https://github.com/boostorg/boost/releases/download/boost-1.85.0/${BOOST_DOWNLOAD_NAME}.tar.xz" -O "${BOOST_VERSION}.tar.xz" --no-check-certificate
