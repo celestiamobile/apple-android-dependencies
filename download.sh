62e2a3bc470b208d8d8cc5a89681b5eba7d3da5f
Levin Li <lilinfeng303@outlook.com>
2025-04-13 22:12:47 -0700

    Update dependencies

--- download.sh
+++ download.sh
@@ -16,36 +16,36 @@ wget "https://ftp.gnu.org/gnu/gettext/${GETTEXT_LEGACY_VERSION}.tar.gz" --no-che
 
 wget "https://www.lua.org/ftp/${LUA_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/LuaJIT/LuaJIT/archive/a4f56a459a588ae768801074b46ba0adcfb49eb1.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/LuaJIT/LuaJIT/archive/eec7a8016c3381b949b5d84583800d05897fa960.tar.gz" -O "${LUAJIT_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/anholt/libepoxy/archive/refs/tags/1.5.10.tar.gz" -O "${LIBEPOXY_VERSION}.tar.gz" --no-check-certificate
 
 FMT_DOWNLOAD_NAME="11.1.4.tar.gz"
 wget "https://github.com/fmtlib/fmt/archive/${FMT_DOWNLOAD_NAME}" -O "${FMT_VERSION}.tar.gz" --no-check-certificate
 
-MESHOPTIMIZER_DOWNLOAD_NAME="v0.22.tar.gz"
+MESHOPTIMIZER_DOWNLOAD_NAME="v0.23.tar.gz"
 wget "https://github.com/zeux/meshoptimizer/archive/refs/tags/${MESHOPTIMIZER_DOWNLOAD_NAME}" -O "${MESHOPTIMIZER_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/${EIGEN_VERSION}.tar.gz" --no-check-certificate
 
-ICU_DOWNLOAD_NAME="icu4c-76_1-src.tgz"
-wget "https://github.com/unicode-org/icu/releases/download/release-76-1/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz" --no-check-certificate
+ICU_DOWNLOAD_NAME="icu4c-77_1-src.tgz"
+wget "https://github.com/unicode-org/icu/releases/download/release-77-1/${ICU_DOWNLOAD_NAME}" -O "${ICU_VERSION}.tgz" --no-check-certificate
 
 # wget "https://github.com/google/breakpad/tarball/e92bea30759edbae08205bccd14dc25bf1806f93" -O "${BREAKPAD_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp" --no-check-certificate
+wget "https://github.com/nlohmann/json/releases/download/v3.12.0/json.hpp" --no-check-certificate
 
 wget "https://zlib.net/${ZLIB_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/b53ca669bea4715b6d5fa53c459f47a1fecd7944" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/KhronosGroup/OpenGL-Registry/tarball/ca62982097ebfc119678dc799261ca2ca7800798" -O "${OPENGL_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/KhronosGroup/EGL-Registry/tarball/29c4314e0ef04c730992d295f91b76635019fbba" -O "${EGL_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/KhronosGroup/EGL-Registry/tarball/a7fb4ec38a785cb5914b5a8ba5f264450677338d" -O "${EGL_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.1.0/${JPEG_TURBO_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://codeload.github.com/mackron/miniaudio/tar.gz/refs/tags/0.11.22" -O "${MINIAUDIO_VERSION}.tar.gz" --no-check-certificate
 
-wget "https://github.com/libsdl-org/SDL/releases/download/release-2.32.0/${SDL2_VERSION}.tar.gz" --no-check-certificate
+wget "https://github.com/libsdl-org/SDL/releases/download/release-2.32.4/${SDL2_VERSION}.tar.gz" --no-check-certificate
 
 wget "https://github.com/nih-at/libzip/releases/download/v1.11.3/${LIBZIP_VERSION}.tar.gz" --no-check-certificate
 
@@ -53,5 +53,5 @@ wget "https://github.com/nih-at/libzip/releases/download/v1.11.3/${LIBZIP_VERSIO
 
 # wget "https://storage.googleapis.com/aom-releases/${AOM_VERSION}.tar.gz" -O "${AOM_VERSION}.tar.gz" --no-check-certificate
 
-BOOST_DOWNLOAD_NAME="boost-1.87.0-cmake"
-wget "https://github.com/boostorg/boost/releases/download/boost-1.87.0/${BOOST_DOWNLOAD_NAME}.tar.xz" -O "${BOOST_VERSION}.tar.xz" --no-check-certificate
+BOOST_DOWNLOAD_NAME="boost-1.88.0-cmake"
+wget "https://github.com/boostorg/boost/releases/download/boost-1.88.0/${BOOST_DOWNLOAD_NAME}.tar.xz" -O "${BOOST_VERSION}.tar.xz" --no-check-certificate
